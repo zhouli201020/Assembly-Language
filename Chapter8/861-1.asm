29ccdb7767d01c1187b04feb9855b49af8ba602f
zhouli201020 <zhouli201020@gmail.com>
2022-11-18 11:24:13 +0800

    完善了注释

--- Chapter8/861-1.asm
+++ Chapter8/861-1.asm
@@ -4,7 +4,7 @@
 .586
 DATA SEGMENT USE16
     MESG   DB 'HELLO!',0DH,0AH,'$'
-    OLD1C  DD ?
+    OLD1C  DD ?                       ;保存旧1CH型中断向量
     ICOUNT DB 18                      ;终端计数初值
     COUNT  DB 10                      ;显示行数控制
 DATA ENDS
@@ -20,32 +20,32 @@ CODE SEGMENT USE16
             STI                           ;开中断
     SCAN:   
             CMP    COUNT,0
-            JNZ    SCAN
-            CLI
+            JNZ    SCAN                   ;是否显示了10行'HELLO!'，否则继续扫描
+            CLI                           ;显示10行完成后，关中断
             CALL   RESET
             STI
-            MOV    AH,4CH
+            MOV    AH,4CH                 ;返回DOS
             INT    21H
     ;-----------------------------------------------
-SERVICE PROC
-            PUSHA
-            PUSH   DS
+SERVICE PROC                              ;中断服务子程序
+            PUSHA                         ;将所有通用寄存器的内容压入堆栈，从而保护现场
+            PUSH   DS                     ;DS=40H
             MOV    AX,DATA
-            MOV    DS,AX
-            DEC    ICOUNT
-            JNZ    EXIT
-            MOV    ICOUNT,18
-            DEC    COUNT
-            MOV    AH,9
+            MOV    DS,AX                  ;重新定义用户数据段
+            DEC    ICOUNT                 ;中断计数，每次减1
+            JNZ    EXIT                   ;中断不满18次则跳转，退出中断服务子程序
+            MOV    ICOUNT,18              ;中断满18次则重新给ICOUNT赋值18
+            DEC    COUNT                  ;并且显示行数减1
+            MOV    AH,9                   ;显示字符串
             LEA    DX,MESG
             INT    21H
     EXIT:   
-            POP    DS
+            POP    DS                     ;恢复现场
             POPA
             IRET                          ;返回系统08H型终端服务子程序
 SERVICE ENDP
     ;-----------------------------------------------
-READ1C PROC
+READ1C PROC                               ;转移系统1CH型中断向量
             MOV    AX,351CH
             INT    21H
             MOV    WORD PTR OLD1C,BX
@@ -53,18 +53,18 @@ READ1C PROC
             RET
 READ1C ENDP
     ;-----------------------------------------------
-WRITE1C PROC
-            PUSH   DS
-            MOV    AX,CODE
+WRITE1C PROC                              ;写入用户1CH型中断向量
+            PUSH   DS                     ;将原先DATA段的地址压栈
+            MOV    AX,CODE                ;将CODE段的地址赋给DS
             MOV    DS,AX
-            MOV    DX,OFFSET SERVICE
-            MOV    AX,251CH
+            MOV    DX,OFFSET SERVICE      ;将中断服务子程序入口的偏移地址赋给DX
+            MOV    AX,251CH               ;DOS调用，写入中断向量
             INT    21H
-            POP    DS
+            POP    DS                     ;将原先DATA段的地址重新赋给DS
             RET
 WRITE1C ENDP
     ;-----------------------------------------------
-RESET PROC
+RESET PROC                                ;恢复系统1CH型中断向量
             MOV    DX,WORD PTR OLD1C
             MOV    DS,WORD PTR OLD1C+2
             MOV    AX,251CH
